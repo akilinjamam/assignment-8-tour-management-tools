ff06bf26a4578b5bae51b468f0a184be4946f347
akilinjamam <akilinjamam@gmail.comm>
2023-01-26 17:18:43 +0600

    file-uploaded

--- routes/v1/tours.route.js
+++ routes/v1/tours.route.js
@@ -8,6 +8,11 @@ routers.route('/')
     .post(toursController.saveTours)
     .get(toursController.getTours)
 
+routers.route('/trending')
+    .get(toursController.getTopViewedId)
+routers.route('/cheapest')
+    .get(toursController.getCheapestTours)
+
 routers.route('/:id')
     .get(toursController.getToursById)
     .patch(toursController.updateToursById)
